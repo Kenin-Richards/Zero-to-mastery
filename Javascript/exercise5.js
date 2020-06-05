839b912af16bea011401081defa31f6f862e6c07
Kenin-Richards <kenin.h.richards@gmail.com>
2020-06-05 09:25:08 -0600

    Change checkDriverAge function

--- Javascript/exercise5.js
+++ Javascript/exercise5.js
@@ -1,4 +1,5 @@
-var age = prompt("What is your age?");
+function checkDriverAge() {
+    var age = prompt("How old are you?");
 if (Number(age) < 18) {
 	alert("Sorry, you are too young to drive this car. Powering off");
 } else if (Number(age) > 18) {
@@ -6,4 +7,4 @@ if (Number(age) < 18) {
 } else if (Number(age) === 18) {
 	alert("Congratulations on your first year of driving. Enjoy the ride!");
 }
-
+}

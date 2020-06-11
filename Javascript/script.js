7c08e93434c7f1b0e50932bfc4d005513929fd39
Kenin-Richards <kenin.h.richards@gmail.com>
2020-06-10 18:11:18 -0600

    Make an object

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,8 @@
-var list = ["tiger", "cat", "bear", "bird"];
+var user = {
+    name: "John",
+    age: 34,
+    hobby: "Soccer",
+    isMarried: false,
+};
+
+var list = ["apple", "banana", "orange"];

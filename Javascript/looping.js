df704034453181ade392ddbc49656596cce229f1
Kenin-Richards <kenin.h.richards@gmail.com>
2020-06-17 13:53:08 -0600

    looping exercise

--- Javascript/looping.js
+++ Javascript/looping.js
@@ -5,5 +5,24 @@ var todos = [
     "study javascript",
     "eat healthy",
 ];
+var todosLength = todos.length;
+for (var i=0; i < todosLength; i++) {
+    console.log(i);
+}
 
-for ()
+todos.forEach(function(i) {
+    console.log(i);
+})
+
+// var counterOne = 10;
+// while (counterOne > 0) {
+//     console.log(counterOne);
+//     counterOne--;
+// }
+
+// var counterTwo = 10
+// do {
+//     console.log(counterTwo);
+//     counterTwo--;
+// } while (counterTwo > 0);
+

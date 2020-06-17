a4bda3566d5a16585a9231f6cd7122a9b0899668
Kenin-Richards <kenin.h.richards@gmail.com>
2020-06-17 14:29:17 -0600

    facebook with looping

--- Javascript/facebook.js
+++ Javascript/facebook.js
@@ -2,7 +2,15 @@ var database = [
     {
         userName: "Kenji",
         password: "Beepboop",
-    }
+    },
+    {
+        userName: "sally",
+        password: "123",
+    },
+    {
+        userName: "ingrid",
+        password: "777",
+    },
 ];
 
 var newsFeed = [
@@ -20,16 +28,34 @@ var newsFeed = [
     }
 ];
 
+function isUserValid(userName, password) {
+    for (var i=0; i < database.length; i++) {
+        if(database[i].userName === userName &&
+            database[i].password === password) {
+                return true;
+            }
+
+    }
+    return false;
+}
+
+function signIn(userName, password) {
+        if(isUserValid(userName, password)) {
+                console.log(newsFeed);
+            } else {
+                alert("Sorry, wrong username and password");
+            }
+
+    }
+//     if (user === database[0].userName && 
+//         pass === database[0].password) { 
+//             console.log(newsFeed);
+//         } else {
+//             alert("Sorry, wrong username and password");
+//         }
+// }
+
 var userNamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password?");
-
-function signIn(user, pass) {
-    if (user === database[0].userName && 
-        pass === database[0].password) { 
-            console.log(newsFeed);
-        } else {
-            alert("Sorry, wrong username and password");
-        }
-}
 
 signIn(userNamePrompt, passwordPrompt);

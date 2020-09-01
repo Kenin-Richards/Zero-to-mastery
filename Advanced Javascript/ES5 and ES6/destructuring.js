030b6e64140c100c58c2d8d3a4027c95675a750b
Kenin-Richards <kenin.h.richards@gmail.com>
2020-09-01 08:59:33 -0600

    destructuring

--- Advanced Javascript/ES5 and ES6/destructuring.js
+++ Advanced Javascript/ES5 and ES6/destructuring.js
@@ -1,12 +1,29 @@
-const obj = {
-    player: 'bobby',
-    experience: 100,
-    wizardLevel: false
+// template strings
+
+// const name = "sally";
+// const age = 34;
+// const pet = "horse";
+
+
+// const greetingBest = `Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have.`
+
+//default arguments
+
+// function greet(name='', age=30, pet='cat') { 
+//     return`Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have`;
+
+// }
+
+//Symbol
+
+// let sym1 = Symbol();
+// let sym2 = Symbol('foo');
+// let sym3 = Symbol('foo')
+
+//arrow functions
+
+function add(a, b) {
+    return a +b;
 }
 
-const player = obj.player;
-const experience = obj.experience;
-let wizardLevel = obj.wizardLevel;
-
-const { player, experience } = obj;
-let { wizardLevel } = obj;
+const add2 = (a, b) => a + b;
